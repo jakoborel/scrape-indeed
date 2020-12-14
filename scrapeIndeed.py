72158cd2f2f06163525bb1c3886407a612561a65
Jakob Orel <jorel22@cornellcollege.edu>
2020-12-13 23:54:43 -0600

    scrape company names

--- scrapeIndeed.py
+++ scrapeIndeed.py
@@ -15,11 +15,23 @@ soup = BeautifulSoup(page.text, "html.parser")
 # print out the pretty html
 #print(soup.prettify())
 
+# Extract all job titles from the page
 def extract_job_title_from_result(soup):
-  jobs = []
-  for h2 in soup.find_all(name="h2", attrs={"class":"title"}):
-      for a in h2.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
-          jobs.append(a["title"])
-  return(jobs)
+    jobs = []
+    for h2 in soup.find_all(name="h2", attrs={"class":"title"}):
+        for a in h2.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
+            jobs.append(a["title"])
+    return(jobs)
 
-print(extract_job_title_from_result(soup))
+# print(extract_job_title_from_result(soup))
+
+# Extract company name from the page
+def extract_company_from_result(soup):
+    companies = []
+    for div in soup.find_all(name="div", attrs={"class":"sjcl"}):
+        company = div.find_all(name="span", attrs={"class":"company"})
+        for b in company:
+            companies.append(b.text.strip())
+    return(companies)
+
+# print(extract_company_from_result(soup))
