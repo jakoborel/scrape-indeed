09a8574556815ea1396eda6528684fc0452eb530
Jakob Orel <jorel22@cornellcollege.edu>
2021-02-01 23:16:15 -0600

    fix: fixed order of location inserting in list

--- scrapeIndeed.py
+++ scrapeIndeed.py
@@ -6,7 +6,9 @@ import pandas as pd
 import time
 
 # The URL can be adjusted to change the query for jobs
-URL = "https://www.indeed.com/jobs?q=data%20analyst&l=Omaha%2C%20NE&vjk=32a6ca0100d10ed7"
+
+# If comparing in browser, make sure to clear cookies.
+URL = "https://www.indeed.com/q-data-analyst-l-Omaha,-NE-jobs.html"
 
 # request the URL
 page = requests.get(URL)
@@ -24,6 +26,7 @@ def extract_job_title_from_result(soup):
             jobs.append(a["title"])
     return(jobs)
 print("Jobs:")
+print(len(extract_job_title_from_result(soup)))
 print(extract_job_title_from_result(soup))
 
 # Extract company name from the page
@@ -35,17 +38,22 @@ def extract_company_from_result(soup):
             companies.append(b.text.strip())
     return(companies)
 print("Companies:")
+print(len(extract_company_from_result(soup)))
 print(extract_company_from_result(soup))
 
-# gets all locations but in wrong order
-# TO-DO: add if/else so that it is added in the right order.
+# Extract location from the page
 def extract_location_from_result(soup):
     locations = []
-    for div in soup.find_all(name="div", attrs={"class":"location"}):
-        locations.append(div.text)
-    for span in soup.find_all(name="span", attrs={"class":"location"}):
-        locations.append(span.text)
-    return(locations)
+    for div in soup.find_all(name="div", attrs={"class":"sjcl"}):
 
+        try:
+            locations.append(div.find(name="div", attrs={"class":"location"}).text)
+            #print("no exception")
+        except Exception as e:
+            #print(str(e))
+            locations.append(div.find(name="span", attrs={"class":"location"}).text)
+    
+    return(locations)
 print("Locations:")
+print(len(extract_location_from_result(soup)))
 print(extract_location_from_result(soup))
