a9bd5d6029cb4f38e40315dd919689dc24d6e250
Jakob Orel <jorel22@cornellcollege.edu>
2021-02-07 23:39:49 -0600

    feat: cleaned some data and looked at salary

--- Data Analysis/exploreWA.py
+++ Data Analysis/exploreWA.py
@@ -3,3 +3,8 @@ import pandas as pd
 df = pd.read_csv("data/Data_Job_WA.csv")
 
 print(df.head())
+#print(df.describe())
+
+# Print median salary for "data scientist" positions in WA state
+print("Median Min_Salary: ", df["Min_Salary"].median())
+print("Median Max_Salary: ", df["Max_Salary"].median())
